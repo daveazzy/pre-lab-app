3ac698eed35ff35c864075a248754816d8aff9df
daveazzy <davidsonoliveira47@gmail.com>
2026-08-16 15:37:24 -0300

    creating static (yet)  users component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('pre-lab-app');
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { App } from './app/app.component';
 import { HeaderComponent } from './app/header/header.component';
 
 bootstrapApplication(App, appConfig)
