1d870bd4e07bb908efb28ab6e7551e27ceace0a4
daveazzy <davidsonoliveira47@gmail.com>
2026-08-19 21:00:24 -0300

    leeeets goooo flamengo

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,18 @@
 import { Component, input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   name = input.required<string>()
   userId = input.required<string>()
+
+  isAddingTask: boolean = false
 
   tasks = [
   {
@@ -43,5 +46,10 @@ export class TasksComponent {
 
   onCompleteTask (id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+
+  onStartAddTask () {
+    this.isAddingTask = true
   }
 }
